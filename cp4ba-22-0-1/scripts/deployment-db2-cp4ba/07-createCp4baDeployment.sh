cd147aa2d7c5602926e33c6ea403f46b2e161606
David Cavanaugh <dcavanau@us.ibm.com>
2022-10-24 13:24:22 -0400

    remove tagged images

--- cp4ba-22-0-1/scripts/deployment-db2-cp4ba/07-createCp4baDeployment.sh
+++ cp4ba-22-0-1/scripts/deployment-db2-cp4ba/07-createCp4baDeployment.sh
@@ -286,7 +286,7 @@ echo "Creating secret ibm-entitlement-key in project $cp4baProjectName..."
 # oc project ibm-common-services
 oc project $cp4baProjectName
 oc create secret docker-registry ibm-entitlement-key --docker-server=${DOCKER_REG_SERVER} --docker-username=${DOCKER_REG_USER} --docker-password=${ENTITLEMENTKEY} --docker-email=${EMAIL} --namespace=$cp4baProjectName
-oc project $cp4baProjectName
+#oc project $cp4baProjectName
 echo "Done."
 
 echo
